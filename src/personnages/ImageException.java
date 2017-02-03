fda47f95e893af6fcb606cb57b0ef71ebd6c186a
Andréas Meuleman <andreasmeu@gmail.com>
2017-02-03 10:05:39 +0100

    Update ImageException.java

--- src/personnages/ImageException.java
+++ src/personnages/ImageException.java
@@ -4,7 +4,6 @@ package personnages;
  * Gestion d'exception au chargement d'une image
  */
 class ImageException extends Exception {
-	private static final long serialVersionUID = 1L;
 
 	public ImageException(String s){
 		System.out.println(s+" : Pas d'image correspondante");
